d45a3fee9cb4aeadf1704fc4687d9a9bb99cfb8d
Damien Pinna <damien.pinna@gmail.com>
2020-07-25 16:34:30 +0200

    animation du menu au clic sur le burger menu

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const btnRondMenu = document.querySelector('.btn-rond-menu');
 const containerLignes = document.querySelector('.container-lignes');
-const navgauche = document.querySelector('.nav-gauche');
+const navGauche = document.querySelector('.nav-gauche');
 const allNavMenuItem = document.querySelectorAll('.nav-menu-item');
 
 btnRondMenu.addEventListener('click', () => {
    containerLignes.classList.toggle('active');
+   navGauche.classList.toggle('menu-visible');
 });
+
+if(window.matchMedia('(max-width: 1300px)')) {
+   allNavMenuItem.forEach( item => {
+      item.addEventListener('click', () => {
+         containerLignes.classList.toggle('active');
+         navGauche.classList.toggle('menu-visible');
+      });
+   });
+}
