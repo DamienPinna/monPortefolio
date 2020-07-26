83228e8d7e7fb7f85c10b1bdbbd47e85a6949fcf
Damien Pinna <damien.pinna@gmail.com>
2020-07-26 15:18:46 +0200

    ajout de l'animation d'accueil avec typewriter

--- app.js
+++ app.js
@@ -16,3 +16,34 @@ if(window.matchMedia('(max-width: 1300px)')) {
       });
    });
 }
+
+// Animation écriture
+const texteAnimation = document.querySelector('.texte-animation');
+
+let typewriter = new Typewriter(texteAnimation, {
+   loop: false,
+   deleteSpeed: 20
+});
+
+typewriter
+   .pauseFor(1800)
+   .changeDelay(90)
+   .typeString("Moi c'est Damien PINNA")
+   .changeDelay(70)
+   .typeString("<strong>, Développeur Junior</strong><span style='color: #ea39ff;'> HTML</span> !")
+   .pauseFor(1000)
+   .deleteChars(6)
+   .typeString("<span style='color: #03A9F4;'> CSS</span> !")
+   .pauseFor(1000)
+   .deleteChars(5)
+   .typeString("<span style='color: #d9122b;'> JavaScript</span> !")
+   .pauseFor(1000)
+   .deleteChars(12)
+   .typeString("<span style='color: #287242;'> Vue.js</span> !")
+   .pauseFor(1000)
+   .deleteChars(8)
+   .typeString("<span style='color: #349717;'> Node.js</span> !")
+   .pauseFor(1000)
+   .deleteChars(9)
+   .typeString("<span style='color: #ff6910;'> Full-Stack JS !")
+   .start();
